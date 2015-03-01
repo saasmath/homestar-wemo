c84895ab8fbc1a5ec7dc602e37792587c48c5db8
David Janes <davidjanes@davidjanes.com>
2015-03-01 07:47:42 -0500

    Stubbed in Insight

--- models/WeMoInsight.js
+++ models/WeMoInsight.js
@@ -1,28 +1,34 @@
 /*
- *  WeMoSocket.js
+ *  WeMoInsight.js
  *
  *  David Janes
  *  IOTDB
- *  2014-01-26
+ *  2014-03-01
  */
 
 var iotdb = require("iotdb");
 
-exports.Model = iotdb.make_model('WeMoSocket')
+exports.Model = iotdb.make_model('WeMoInsight')
     .facet(":plug")
     .facet(":switch")
-    .product("http://www.belkin.com/us/F7C027-Belkin/p/P-F7C027/")
-    .name("WeMo Socket")
-    .description("Belkin WeMo Socket")
+    .facet(":sensor")
+    .product("http://www.belkin.com/us/support-product?pid=01t80000003JS3FAAW")
+    .name("WeMo Insight")
+    .description("Belkin WeMo Insight")
     .io("on", iotdb.boolean.on)
+    .i("today-kwh", iotdb.number)
+    .i("current-power", iotdb.number)
+    .i("today-on-time", iotdb.number)
+    .i("on-for", iotdb.number)
+    .i("today_standby-time", iotdb.number)
     .make();
 
 exports.binding = {
     bridge: require('../WeMoBridge').Bridge,
     model: exports.Model,
     matchd: {
-        'iot:vendor/type': 'urn:Belkin:device:controllee:1',
-        'iot:vendor/model': 'Socket',
+        'iot:vendor/type': 'urn:Belkin:device:insight:1',
+        'iot:vendor/model': 'Insight',
     },
     connectd: {
         subscribes: [
@@ -38,6 +44,15 @@ exports.binding = {
                     paramd.cookd.on = false;
                 }
             }
+
+            console.log("NEW VALUES HERE", paramd);
+            /*
+            paramd.cooked["today-kwh"] = parseInt(some_value);
+            paramd.cooked["current-power"] = parseInt(some_value);
+            paramd.cooked["today-on-time"] = parseInt(some_value);
+            paramd.cooked["on-for"] = parseInt(some_value);
+            paramd.cooked["today_standby-time"] = parseInt(some_value);
+            */
         },
 
         data_out: function(paramd) {
--- models/WeMoMotion.js
+++ models/WeMoMotion.js
@@ -1,5 +1,5 @@
 /*
- *  WeMoSocketMotion.js
+ *  WeMoSocket.js
  *
  *  David Janes
  *  IOTDB
